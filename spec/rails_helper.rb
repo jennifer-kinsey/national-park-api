bd7b1d8677b1050a2c45b71add7125e99281a980
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-07-21 12:14:12 -0700

    adds first api tests for getting /parks

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-FactoryGirl.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
-FactoryGirl.find_definitions
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
