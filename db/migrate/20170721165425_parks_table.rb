35db3e147b42b9687b29b8f2a7e75a58a6a871de
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-07-21 10:19:16 -0700

    add parks table migration

---
+++ db/migrate/20170721165425_parks_table.rb
@@ -0,0 +1,19 @@
+class ParksTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :parks do |t|
+      t.string :name
+      t.string :state
+      t.text :mailingaddress
+      t.string :phone
+      t.text :description
+      t.integer :fee
+      t.string :open_through
+      t.string :closure
+      t.string :amenities
+      t.string :activities
+      t.decimal :sq_mi
+
+      t.timestamps
+    end
+  end
+end
