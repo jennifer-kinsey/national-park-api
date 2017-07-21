c9bc2a689cbc8e83c00ac45a890b48bd6c1dd118
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-07-21 11:03:46 -0700

    fix to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,17 @@ require 'faker'
 Park.destroy_all
 
 250.times do |i|
-  park = FactoryGirl.create!(
-    name: Faker::StarTrek.location + "National Park"
-    state: Faker::Address.state
-    mailingaddress: "PO BOX " + Faker::Address.building_number + " Faker::Address.city " + state
-    phone: "(" + Faker::Number.number(3) + ") " + Faker::Number.number(3) + "-" + Faker::Number.number(4)
-    description: Faker::Lorem.paragraph
-    fee: Faker::Number.number(1)
-    open_through: Faker::Date.forward(365)
-    closure: Faker::Address.street_name + " Trail"
-    amenities: Faker::Lorem.sentences(1)
-    activities: Faker::Lorem.sentences(1)
+  park = FactoryGirl.create(
+    name: Faker::StarTrek.location + "National Park",
+    state: Faker::Address.state,
+    mailingaddress: "PO BOX " + Faker::Address.building_number + " " + Faker::Address.city + " " + Faker::Address.state,
+    phone: "(" + Faker::Number.number(3) + ") " + Faker::Number.number(3) + "-" + Faker::Number.number(4),
+    description: Faker::Lorem.paragraph,
+    fee: Faker::Number.number(1),
+    open_through: Faker::Date.forward(365),
+    closure: Faker::Address.street_name + " Trail",
+    amenities: Faker::Lorem.sentences(1),
+    activities: Faker::Lorem.sentences(1),
     sq_mi: Faker::Number.decimal(3, 3)
   )
 end
