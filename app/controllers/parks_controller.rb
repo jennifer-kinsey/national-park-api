ce2b8858b51e72f28833d1b241b8d728a60f6ac4
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-07-21 11:59:05 -0700

    adds Handling Exceptions and Adding Success Messages

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,28 +1,36 @@
 class ParksController < ApplicationController
 
   def index
-    @parks =Park.all
+    @parks = Park.all
     json_response(@parks)
   end
 
   def show
-    @park =Park.find(params[:id])
+    @park = Park.find(params[:id])
     json_response(@park)
   end
 
   def create
-    @park =Park.create(park_params)
-    json_response(@park)
+    @park = Park.create!(park_params)
+    json_response(@park, :created)
   end
 
   def update
-    @park =Park.find(params[:id])
-    @park.update(park_params)
+    @park = Park.find(params[:id])
+    if @park.update!(park_params)
+      render status: 200, json: {
+        message: "Your park has been successfully updated."
+      }
+    end
   end
 
   def destroy
-    @park =Park.find(params[:id])
-    @park.destroy
+    @park = Park.find(params[:id])
+    if @park.destroy
+      render status: 200, json: {
+        message: "Your park has been deleted."
+      }
+    end
   end
 
   private
