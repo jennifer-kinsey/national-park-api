529d625ccda511fdb72da07c13c4ec100373cae5
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-07-21 10:49:42 -0700

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Park.destroy_all
+
+250.times do |i|
+  park = FactoryGirl.create!(
+    name: Faker::StarTrek.location + "National Park"
+    state: Faker::Address.state
+    mailingaddress: "PO BOX " + Faker::Address.building_number + " Faker::Address.city " + state
+    phone: "(" + Faker::Number.number(3) + ") " + Faker::Number.number(3) + "-" + Faker::Number.number(4)
+    description: Faker::Lorem.paragraph
+    fee: Faker::Number.number(1)
+    open_through: Faker::Date.forward(365)
+    closure: Faker::Address.street_name + " Trail"
+    amenities: Faker::Lorem.sentences(1)
+    activities: Faker::Lorem.sentences(1)
+    sq_mi: Faker::Number.decimal(3, 3)
+  )
+end
