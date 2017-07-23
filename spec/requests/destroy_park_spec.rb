16f8c90b7553f11d680bb5f377759fc424b62920
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-07-23 09:17:56 -0700

    fixes destroy park spec

--- spec/requests/destroy_park_spec.rb
+++ spec/requests/destroy_park_spec.rb
@@ -3,20 +3,22 @@ require 'rails_helper'
 describe 'parks#destroy', :type => :request do
   before do
     @park = FactoryGirl.create(:park)
-    @user = User.create(
-      email: 'randy3@email.com',
-      password: 'password',
-      password_confirmation: 'password'
-    )
-    delete "/parks/#{@park.id}"
+    user = FactoryGirl.create(:user)
+    post '/auth_user', params: {
+      email: user.email,
+      password: user.password
+    }
+    @user_api_key = JSON.parse(response.body)["auth_token"]
   end
 
   it 'deletes the park' do
+    delete "/parks/#{@park.id}?api_key=#{@user_api_key}"
     expect(response).to have_http_status(200)
     expect(JSON.parse(response.body)['message']).to eq("Your park has been deleted.")
   end
 
   it 'responds with 404' do
+    @park.destroy
     get "/parks/#{@park.id}"
     expect(response).to have_http_status(404)
   end
